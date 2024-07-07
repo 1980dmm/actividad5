7c5b89b61008ea8c9df2f25ff7fa38686d3685eb
Daniel <daniel.martinez.moreno@gmail.com>
2024-07-07 17:23:11 +0200

    Comunicación formulario y presentación

--- appAct5/src/app/components/blog/blog.component.ts
+++ appAct5/src/app/components/blog/blog.component.ts
@@ -14,13 +14,34 @@ export class BlogComponent {
   arrayNoticias: INoticia[] =[];
   mensajeError: string = "";
 
+  constructor(){
+    this.arrayNoticias = [
+      {title: "EEUU sigue por detrás de China en patentes de IA generativa: el país asiático lidera este ámbito de manera aplastante",
+       image:"https://i.blogs.es/ecfd98/1inteligencia-artificial-china-portada/1200_800.jpeg",
+       body:"Si bien Estados Unidos es el hogar de algunas de las compañías de inteligencia artificial generativa (GenAI) más conocidas del momento, China lidera en cantidad de invenciones. El último informe de Organización Mundial de la Propiedad Intelectual (OMPI) señala que desde 2017 el gigante asiático ha publicado más patentes en este campo cada año que todas las demás regiones del planeta juntas.",
+       date:"2024-07-05" 
+      },
+      {title:"El robot humanoide de Figure ya es capaz de trabajar de manera autónoma en una fábrica, así que lo está haciendo en BMW",
+        image:"https://i.blogs.es/caf55b/figure-bmw-3/1200_800.jpeg",
+        body:"Estamos presenciando en directo como la robótica evoluciona y, de alguna u otra forma, se entrelaza con nuestras vidas. Aún no podemos ir a una tienda a comprar androides avanzados al más puro estilo ‘Detroit Become Human’, pero probablemente un aspirador Roomba, Conga o de otra marca nos ayuda a mantener limpio el suelo de casa. En paralelo, la mayor parte de los coches que nos rodean seguramente provienen de líneas de producción altamente robotizadas.",
+        date:"2024-07-04"
+
+      }
+
+    ] 
+  }
+
 publicar(){
   // primero borrar el mensaje erróneo (por si estuviera mostrándose)
   this.limpiarError()
   // si la noticia se puede agregar, lo hacemos y la reiniciamos
   if (this.checkNewNoticia()){
     console.log("Agregamos y reiniciamos")
+    console.log(this.newNoticia)
+    this.arrayNoticias.push(this.newNoticia);
+    console.log(this.arrayNoticias)
     this.clearNoticia()  
+    this.cargarBlog()
   } else {
   // si no sep puede agregar mostramos el mensaje de error
   this.mostrarError()
@@ -30,10 +51,7 @@ publicar(){
 }
 
 clearNoticia(){
-  this.newNoticia.body = "";
-  this.newNoticia.title = "";
-  this.newNoticia.date = "";
-  this.newNoticia.image = "";
+  this.newNoticia = { body: "", title: "", date: "", image:"" }
 }
 
 checkNewNoticia(): boolean{
@@ -55,6 +73,19 @@ mostrarError(){
 
 limpiarError(){
   this.mensajeError="";
+}
+
+cargarBlog(): string{
+  let html: string = "";
+  this.arrayNoticias.forEach((noticia:INoticia) => {
+    //html = html + `<div class="noticia">`
+    html = html + `<div><h2>${noticia.title}</h2></div>`
+    html = html + `<div><p>${noticia.date}</p></div>`
+    html = html + `<div><img src="${noticia.image}"></div>`
+    html = html + `<div><p>${noticia.body}</p></div>`
+    //html = html + '</div>'
+  })
+  return html
 }
 
 }
